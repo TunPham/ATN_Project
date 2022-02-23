a401a7eb2417da5732ad6f8d15152f2d307d34a5
Tuan <tuanpvgcs200073@fpt.edu.vn>
2022-02-24 00:43:50 +0700

    update

--- src/model/products.js
+++ src/model/products.js
@@ -7,13 +7,15 @@ const Product = mongoose.Schema({
     description: {type: String, require: true},
     image: {type: String, require: true},
     quantity:{type: Number, default: 0},
+    categoryId:{ type : String, require: true},
+    brand:{ type: String, require: true},
     slug:{
         type: String, slug: 'name', unique: true,
-    }
+    } // Cơ chế của hoạt động của slug : Lấy name của sản phẩm , sau đó lowercase toàn bộ, dấu cách sẽ được thay bằng " - " và để lên url
 },{
     timestamps: true // Tu dong tao ra ngay thang tao san pham va ngay thang cap nhat gan nhat
 })
 
 mongoose.plugin(slug);
 x
-module.exports = mongoose.model('products', Product) // tham so 1 la ten db, tham so 2 la doi tuong Product duoc tao o tren
+module.exports = mongoose.model('products', Product) // tham so 1 la ten table ( ở đây là products), tham so 2 la doi tuong Product duoc tao o tren dòng 4
---
+++ src/model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator') 
+
+const User = mongoose.Schema({
+    userName: {type: String, require:true },
+    password: {type: String,require: true},
+    author: {type: String,require: true},
+    
+    slug:{
+        type: String, slug:'name', unique: true,
+    }
+})
