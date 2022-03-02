644f108a755bc4d72cc182f60627a7d9a8c8e2e6
Otealong <tricuong777@gmail.com>
2022-03-02 17:06:39 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 const route = require('./routes/index')
 const handlebars  = require('express-handlebars');
 const path = require('path')
 const db = require('./configDB/connectDB')
 
-
 route(app);
 db.connect();
 
 app.use(express.json())
+app.use(express.static(path.join(__dirname,'public')))
 
 app.engine('handlebars', handlebars.engine());
 app.set('view engine', 'handlebars');
